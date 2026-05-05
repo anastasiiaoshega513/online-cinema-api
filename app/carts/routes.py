3d36fee7e2cafe54173ca3cfe4190768fd46d2aa
Steicy <anastasiaosega@gmail.com>
2026-05-05 15:40:24 +0300

    Implement adding movie to cart endpoint

--- app/carts/routes.py
+++ app/carts/routes.py
@@ -1,37 +1,22 @@
-from fastapi import APIRouter, Header, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import selectinload
 
 from app.accounts.dependencies import get_current_user
 from app.accounts.models import User
 from app.carts.models import Cart, CartItem
 from app.carts.schemas import CartResponseScheme
+from app.carts.services import get_or_create_cart, get_cart_with_items
 from db.dependencies import get_db
+from app.movies.models import Movie
 
 router = APIRouter()
 
-# POST /cart/items/{movie_id}
 # DELETE /cart/items/{movie_id}
 # DELETE /cart/clear
 
 @router.get("/cart/", response_model=CartResponseScheme, response_model_exclude_none=True)
-async def get_cart(current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
-    result = await db.execute(
-        select(Cart)
-        .options(
-            selectinload(Cart.items).selectinload(CartItem.movie)
-        )
-        .where(Cart.user_id == current_user.id)
-    )
-
-    cart = result.scalar_one_or_none()
-
-    if cart is None:
-        cart = Cart(user_id=current_user.id)
-        db.add(cart)
-        await db.commit()
-
+async def get_cart(cart: Cart = Depends(get_or_create_cart)):
     if not cart.items:
         return {"detail": "There are no cart items yet."}
 
@@ -39,5 +24,37 @@ async def get_cart(current_user: User = Depends(get_current_user), db: AsyncSess
 
 
 @router.post("/cart/items/{movie_id}/", response_model=CartResponseScheme)
-async def add_to_cart(movie_id: int, current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+async def add_to_cart(movie_id: int, cart: Cart = Depends(get_or_create_cart), current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+
+    movie = await db.scalar(
+        select(Movie).where(Movie.id == movie_id)
+    )
+
+    if not movie:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Movie not found.",
+        )
+
+    stmt = await db.execute(
+        select(CartItem).where(
+            CartItem.cart_id == cart.id,
+            CartItem.movie_id == movie_id,
+        )
+    )
+    existing_item = stmt.scalar_one_or_none()
+
+    if existing_item:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Movie is already in the cart.")
+
+    item = CartItem(movie_id=movie_id, cart_id=cart.id)
+    db.add(item)
+    await db.commit()
+
+    cart = await get_cart_with_items(db=db, user_id=current_user.id)
+
+    return {
+        "detail": "Movie has been added to the cart.",
+        "items": cart.items,
+    }
 
---
+++ app/carts/services.py
@@ -0,0 +1,42 @@
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from app.accounts.dependencies import get_current_user
+from app.accounts.models import User
+from app.carts.models import Cart, CartItem
+from db.dependencies import get_db
+
+
+async def get_cart_with_items(db: AsyncSession, user_id: int) -> Cart | None:
+    result = await db.execute(
+        select(Cart)
+        .options(
+            selectinload(Cart.items).selectinload(CartItem.movie)
+        )
+        .where(Cart.user_id == user_id)
+    )
+
+    return result.scalar_one_or_none()
+
+
+async def get_or_create_cart(db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    result = await db.execute(
+        select(Cart)
+        .options(
+            selectinload(Cart.items).selectinload(CartItem.movie)
+        )
+        .where(Cart.user_id == current_user.id)
+    )
+
+    cart = result.scalar_one_or_none()
+
+    if cart is None:
+        cart = Cart(user_id=current_user.id)
+        db.add(cart)
+        await db.commit()
+
+        cart = await get_cart_with_items(db=db, user_id=current_user.id)
+
+    return cart
